b98bc1ff2b94870d63e2aaf6012f142a89669be1
dalania <jdalaniasilva@gmail.com>
2021-12-06 09:10:30 -0300

    🎨 finalizado coleta

--- src/reclameAqui.py
+++ src/reclameAqui.py
@@ -3,9 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+from peewee import *
+from peeweeOrm.entities.Games import *
+from peeweeOrm.entities.StatisticsGames import *
+from strategies.VerificaGreenCantoByScoreBing import *
+from selenium.webdriver.support import expected_conditions as EC
+from strategies.VerificaGreenCantoNewVersionFunilScoreBing import *
 import os
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+
 
 
 
@@ -16,23 +22,13 @@ driver = webdriver.Chrome(options=options,executable_path=DRIVER_PATH)
 
 
 
-lista_lojas = ['magazine-luiza-loja-online','americanas-com-loja-online', ]
+lista_lojas = ['americanas-com-loja-online', ]
 
 lista_dados = []
 
 for loja in lista_lojas:
   
     driver.get(f'https://www.reclameaqui.com.br/empresa/{loja}/')
-
-
-    """ driver.find_element_by_class_name("form-control").send_keys(loja)
-   
-    driver.find_element_by_class_name('btn-blue-dark').click()
-    
-
-    time.sleep(3)
-    print('Chegou aqui')
-    """
     try:
 
         driver.find_element_by_id('onetrust-accept-btn-handler').click()
@@ -43,15 +39,65 @@ for loja in lista_lojas:
     except:
         pass
     
-    """ driver.find_element_by_class_name('wrapper-item').click()
-    time.sleep(5) """
     time.sleep(3)
+    driver.find_element_by_id('reputation-tab-5').click()
 
-    texto = driver.find_element_by_class_name('description')
-    print(texto.text)
+    indices = driver.find_elements_by_class_name('label')
+    total_reclamacoes = driver.find_elements_by_class_name('stats')[1].find_element_by_tag_name('b')
+    reclamacoesRespondidas = driver.find_elements_by_class_name('stats')[2].find_element_by_tag_name('b')
+    reputacao = driver.find_elements_by_class_name('stats')[0].find_element_by_tag_name('b')
+
+
+    percentualRespondidas = int(reclamacoesRespondidas.text)/int(total_reclamacoes.text)
+
+    principaisProblemas = driver.find_elements_by_class_name('nvhcgi-2')
+
+    driver.execute_script("document.querySelectorAll('.chu0q3-0.zUPSt.card-header')[3].click()")
+    driver.execute_script("document.querySelectorAll('.chu0q3-0.zUPSt.card-header')[4].click()")
+    driver.execute_script("document.querySelectorAll('.chu0q3-0.zUPSt.card-header')[5].click()")
+    time.sleep(3)
+    categorias = driver.find_elements_by_class_name('nvhcgi-1.jMSnzo')
+    tiposDeProblemaSubCategoria = []
+    produtosServicos = []
+    problemasSubCategorias = []
+
+    for nomeProblema in categorias[0].find_elements_by_tag_name('a'):
+        nome  = nomeProblema.text.split("\n")[0] 
+        valor = nomeProblema.text.split("\n")[1].replace('(','').replace(')','')
+        tiposDeProblemaSubCategoria.append({nome: valor} )
+
+    for nomeProblema in categorias[1].find_elements_by_tag_name('a'):
+        nome  = nomeProblema.text.split("\n")[0] 
+        valor = nomeProblema.text.split("\n")[1].replace('(','').replace(')','')
+        produtosServicos.append({nome: valor} )
+
+    for nomeProblema in categorias[2].find_elements_by_tag_name('a'):
+        nome  = nomeProblema.text.split("\n")[0] 
+        valor = nomeProblema.text.split("\n")[1].replace('(','').replace(')','')
+        problemasSubCategorias.append({nome: valor} )        
+
+    informacao = {
+                    'Nome Da Loja': loja,
+                    'Total de reclamaçoes': total_reclamacoes.text,
+                    'Reclamações Respondidas' : "{:.1f}%".format(percentualRespondidas * 100),
+                    'Voltariam a fazer negogicio': indices[1].text,
+                    'Indice de soluçao' : indices[2].text,
+                    'Nota do consumidor' :indices[3].text,
+                    'Reputação': reputacao.text,
+                    'Percentual Principais Problemas': {
+                                            principaisProblemas[0].find_element_by_tag_name('a').text.split('%')[1][1::]:f"{principaisProblemas[0].find_element_by_tag_name('a').text.split('%')[0]}%",
+                                            principaisProblemas[1].find_element_by_tag_name('a').text.split('%')[1][1::]:f"{principaisProblemas[1].find_element_by_tag_name('a').text.split('%')[0]}%",
+                                            principaisProblemas[2].find_element_by_tag_name('a').text.split('%')[1][1::]:f"{principaisProblemas[2].find_element_by_tag_name('a').text.split('%')[0]}%",
+                    },
+                    'Principais Problemas':{
+                                            'Tipos de problemas': tiposDeProblemaSubCategoria,
+                                            'Produtos e Servicos': produtosServicos,
+                                            'Categorias': problemasSubCategorias
+                    }
 
     
-    informacao = {'Nome Da Loja': loja, 'Reclamações' : 1}
+        }
     lista_dados.append(informacao)
 time.sleep(5)
-print(lista_dados)
+for a in lista_dados:
+    print(a)
